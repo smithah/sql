35ed2c758e6e54f84bd0c757c09f07a6b5665cd8
smithah <mailsmithah@gmail.com>
2026-05-09 12:30:57 +0530

    Create CryptoMarketAlgorithmsReportQuery.sql
    Crypto Market Algorithms report Query

---
+++ CryptoMarketAlgorithmsReportQuery.sql
@@ -0,0 +1,38 @@
+/*
+Enter your query below.
+Please append a semicolon ";" at the end of the query
+*/
+
+WITH quarterly_volume AS (
+SELECT 
+        algorithm,
+        SUM(volume) as volume,
+        DATEPART(quarter,dt) as quarters
+        
+FROM    coins c
+JOIN    transactions t ON t.coin_code = c.code
+WHERE   DATEPART(year,dt) = 2020
+GROUP BY algorithm, DATEPART(quarter,dt)
+)
+SELECT
+       c.algorithm,
+       qv1.volume as transactions_Q1,
+       qv2.volume as transactions_Q2,
+       qv3.volume as transactions_Q3,
+       qv4.volume as transactions_Q4
+FROM   coins c 
+LEFT JOIN quarterly_volume qv1 
+on c.algorithm = qv1.algorithm
+AND qv1.quarters = 1  
+LEFT JOIN quarterly_volume qv2 
+on c.algorithm = qv2.algorithm
+AND qv2.quarters = 2  
+LEFT JOIN quarterly_volume qv3 
+on c.algorithm = qv3.algorithm
+AND qv3.quarters = 3
+LEFT JOIN quarterly_volume qv4 
+on c.algorithm = qv4.algorithm
+AND qv4.quarters = 4
+WHERE c.code NOT LIKE 'DOGE'
+ORDER BY algorithm;
+go
